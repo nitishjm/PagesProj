0198a62cca44c9146d07fdddc77d493cabdb2cf5
nitishjm <nmallava@sfu.ca>
2020-06-01 20:47:38 -0700

    Added in heroku settings

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^&0nq^&z41y=)2hko*@7%r#uwd09@!&i)^myi&9wliuy=c+y*m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
